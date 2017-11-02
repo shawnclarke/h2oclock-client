2dea9a115174a7262fdd9aeab28fa624bcd9a929
Shawn Clarke <shawn.clarke@arup.com>
2017-11-02 23:15:08 +0000

    added gpio to server.js

--- server.js
+++ server.js
@@ -8,3 +8,16 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
+var Gpio = require('pigpio').Gpio,
+led = new Gpio(11, {mode: Gpio.OUTPUT}),
+dutyCycle = 0;
+
+setInterval(function () {
+led.pwmWrite(dutyCycle);
+
+dutyCycle += 5;
+if (dutyCycle > 255) {
+  dutyCycle = 0;
+}
+}, 20);
