414bf735ac3564b410ff97521fb3457efabce8d1
Shawn Clarke <shawn.clarke@arup.com>
2017-11-03 19:58:42 +0000

    chnage express to serve index.html

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
