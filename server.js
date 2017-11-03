8e6efa75b5eed2affacaf4ab437c4e3bb4a45cc9
Shawn Clarke <shawn.clarke@arup.com>
2017-11-03 19:36:52 +0000

    installed module onoff

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-var Gpio = require('pigpio').Gpio;
-var led = new Gpio(17, {mode: Gpio.OUTPUT});
-
-led.digitalWrite(1);
+var Gpio = require('onoff').Gpio,
+    led = new Gpio(17, 'out');
+ 
+var iv = setInterval(function(){
+	led.writeSync(led.readSync() === 0 ? 1 : 0)
+}, 500);
+ 
+// Stop blinking the LED and turn it off after 5 seconds.
+setTimeout(function() {
+    clearInterval(iv); // Stop blinking
+    led.writeSync(0);  // Turn LED off.
+    led.unexport();    // Unexport GPIO and free resources
+}, 5000);
