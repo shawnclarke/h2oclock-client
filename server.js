6a9164977e10883bf7e9fe82207881d3e87b05c0
Shawn Clarke <shawn.clarke@arup.com>
2017-11-03 20:05:50 +0000

    added path require to server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
