680795353a770744c120794ffc29208e5c616b8e
Shawn Clarke <Shawn.Clarke@arup.com>
2017-11-20 15:54:38 +0000

    completed settings page and js file

--- public/scripts/settings.js
+++ public/scripts/settings.js
@@ -8,10 +8,12 @@ $(function () {
     const $timeSlotsList = $("#timeSlots-list");
     const $submitBtn = $('#submit');
     const $updateBtn = $('#update');
-    const $timeSlotInputType = $('#typeOfDay');
+    const $timeSlotInputTypeOfDay = $('#typeOfDay');
     const $timeSlotInputHour = $('#hour');
     const $timeSlotInputMinute = $('#minute');
     const $timeSlotInputDuration = $('#duration');
+    const $addUpdate = $('#addUpdate');
+    let timeSlotToBeUpdatedId = '';
 
 
     $updateBtn.hide();
@@ -25,7 +27,7 @@ $(function () {
     const getHighTimeSlots = () => {
         $.ajax({
             url: '/timeslots/high'
-        }).done( (response) => {
+        }).done((response) => {
             console.log(response);
             writeTimeSlots(response);
         }).fail(errorHandler);
@@ -34,7 +36,16 @@ $(function () {
     const getMedTimeSlots = () => {
         $.ajax({
             url: '/timeslots/med'
-        }).done( (response) => {
+        }).done((response) => {
+            console.log(response);
+            writeTimeSlots(response);
+        }).fail(errorHandler);
+    }
+
+    const getLowTimeSlots = () => {
+        $.ajax({
+            url: '/timeslots/low'
+        }).done((response) => {
             console.log(response);
             writeTimeSlots(response);
         }).fail(errorHandler);
@@ -42,7 +53,7 @@ $(function () {
 
     const writeTimeSlots = (timeSlots) => {
         $timeSlotsList.html('');
-        timeSlots.forEach( (timeSlot) => {
+        timeSlots.forEach((timeSlot) => {
             let btnUpdate = `<a id="update" class="btn-floating btn-small waves-effect waves-light green" data-id='${timeSlot._id}'><i class="material-icons">edit</i></a>`
             let btnDelete = `<a id="delete" class="btn-floating btn-small waves-effect waves-light red" data-id="${timeSlot._id}"><i class="material-icons">delete</i></a>`
             $timeSlotsList.append(`<tr><td>${timeSlot.typeOfDay}</td><td>${timeSlot.hour} : ${timeSlot.minute}</td><td>${timeSlot.duration}</td><td>${btnUpdate}</td><td>${btnDelete}</td></tr>`)
@@ -54,32 +65,123 @@ $(function () {
             method: 'POST',
             url: '/timeslots',
             data: timeSlot
-        }).done( (response) => {
-            getMedTimeSlots();
-            $timeSlotInputType.val('');
-            $timeSlotInputHour.val('');
-            $timeSlotInputMinute.val('');
-            $timeSlotInputDuration.val('');
-
+        }).done((response) => {
+            switch (timeSlot.typeOfDay) {
+                case 'high':
+                    getHighTimeSlots();
+                    break;
+                case 'med':
+                    getMedTimeSlots();
+                    break;
+                case 'low':
+                    getLowTimeSlots();
+                    break;
+            }
+            $timeSlotInputTypeOfDay.val('')
+            $timeSlotInputHour.val('')
+            $timeSlotInputMinute.val('')
+            $timeSlotInputDuration.val('')
         }).fail(errorHandler);
     }
+
+    const deleteTimeSlot = (idToDelete) => {
+        $.ajax({
+            method: 'DELETE',
+            url: '/timeslots/' + idToDelete
+        }).done((response) => {
+        }).fail(errorHandler);
+    }
+
+    const timeSlotUpdate = (timeSlot) => {
+        $.ajax({
+            method: 'PUT',
+            url: '/timeslots/' + timeSlotToBeUpdatedId,
+            data: timeSlot
+        }).done((response) => {
+            switch (timeSlot.typeOfDay) {
+                case 'high':
+                    getHighTimeSlots();
+                    break;
+                case 'med':
+                    getMedTimeSlots();
+                    break;
+                case 'low':
+                    getLowTimeSlots();
+                    break;
+            }
+            $timeSlotInputTypeOfDay.val('')
+            $timeSlotInputHour.val('')
+            $timeSlotInputMinute.val('')
+            $timeSlotInputDuration.val('')
+        }).fail(errorHandler);
+    }
+
     //Event Bindings
 
     //Get time slots
     $loadHigh.on('click.load', getHighTimeSlots);
     $loadMed.on('click.load', getMedTimeSlots);
+    $loadLow.on('click.load', getLowTimeSlots);
 
-        //Add time slot
-        $submitBtn.on('click', () => {
-            const newTimeSlot = {
-                typeOfDay: $timeSlotInputType.val(),
+    //Add time slot
+    $submitBtn.on('click', () => {
+        const newTimeSlot = {
+            typeOfDay: $timeSlotInputTypeOfDay.val(),
+            hour: $timeSlotInputHour.val(),
+            minute: $timeSlotInputMinute.val(),
+            duration: $timeSlotInputDuration.val() * 1
+        }
+        console.log('newTimeSlot: ', newTimeSlot);
+        addTimeSlot(newTimeSlot);
+        return false;
+    });
+
+    //Delete time slot
+    $($timeSlotsList).on('click.delete', '#delete', function () {
+        const $buttonD = $(this);
+        const timeSlotToBeDeletedId = $buttonD.data('id');
+        console.log('deleting: ', timeSlotToBeDeletedId);
+        deleteTimeSlot(timeSlotToBeDeletedId);
+        $buttonD.closest('tr').remove();
+    });
+
+    //Update time slot - load values in form
+    $($timeSlotsList).on('click.update', '#update', function () {
+        const $buttonU = $(this);
+        timeSlotToBeUpdatedId = $buttonU.data('id');
+        const typeofDayVal = $buttonU.parent().prev().prev().prev().text();
+        const hourVal = $buttonU.parent().prev().prev().text().substr(0, 2);
+        const minuteVal = $buttonU.parent().prev().prev().text().substr(5, 7);
+        const durationVal = $buttonU.parent().prev().text();
+        console.log('Updating: ', timeSlotToBeUpdatedId);
+        $submitBtn.hide();
+        $updateBtn.show();
+        $addUpdate.text('Update a time slot');
+        $timeSlotInputTypeOfDay.val(typeofDayVal);
+        $timeSlotInputTypeOfDay.next().addClass('active');
+        $timeSlotInputHour.val(hourVal);
+        $timeSlotInputHour.next().addClass('active');
+        $timeSlotInputMinute.val(minuteVal);
+        $timeSlotInputMinute.next().addClass('active');
+        $timeSlotInputDuration.val(durationVal);
+        $timeSlotInputDuration.next().addClass('active');
+    });
+
+        //Submit update time slot
+        $updateBtn.on('click', ($e) => {
+            const updateTimeSlot = {
+                _id: timeSlotToBeUpdatedId,
+                typeOfDay: $timeSlotInputTypeOfDay.val(),
                 hour: $timeSlotInputHour.val(),
                 minute: $timeSlotInputMinute.val(),
-                duration: $timeSlotInputDurations.val() *1
+                duration: $timeSlotInputDuration.val() * 1
             }
-            console.log('newTimeSlot', newTimeSlot);
-            //addTimeSlot(newTimeSlot);
-            return false;
-        });
+            console.log('starting to update');
+            timeSlotUpdate(updateTimeSlot);
+            console.log('update: ' + updateTimeSlot);
+           return false;
+       });
+
+
 
 });
--- serverDev.js
+++ serverDev.js
@@ -1,3 +1,4 @@
+const util = require('util');
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -42,10 +43,10 @@ var weatherSchema = new Schema({
 var weather = mongoose.model('weather', weatherSchema);
 
 var timeSlotSchema = new Schema({
+  typeOfDay: String,
   hour: String,
   minute: String,
-  duration: Number,
-  typeOfDay: String
+  duration: Number
 });
 
 var timeSlot = mongoose.model('timeSlot', timeSlotSchema);
@@ -105,6 +106,7 @@ app.get('/timeslots/low', function (req, res) {
 });
 
 app.post('/timeslots', function(req, res){
+
   var newTimeSlot = new timeSlot(req.body);
   newTimeSlot.save(function(err, timeSlot) {
     if(err) {
